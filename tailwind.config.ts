d49992688a55a3a83935d197e738e96ec821c4f3
suntherres111 <suntherres@gmail.com>
2026-03-17 15:11:12 +0800

    Added Safelist into tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+   safelist: [
+    'text-amber-500',
+    'text-amber-600',
+    'text-slate-500',
+    'text-slate-600',
+    // add other shades if needed
+  ],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"
